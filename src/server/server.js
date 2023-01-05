1c164134abc0e35d27b772bc798000da1496152b
Torsten Schlieder <torsten.schlieder@unite.eu>
2023-01-05 19:30:42 +0100

    Minimal GraphQL setup

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,51 @@
 import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import { buildSchema } from 'graphql'
 
-const PORT = 3000
+const PORT = 4000
 const app = express()
 
-app.get('/', (req, res) => {
-    const data = {
-        name: 'Hans',
-        year: 2000,
-        city: 'Rome'
+const schema = buildSchema(`
+    type Person {
+        name: String!
+        year: Int
+        city: String
+        spouse: Int
     }
-    res.json(data)
-})
+    type Query {
+        hello(id: Int!): Person
+    }
+`)
+
+const persons = {
+    '1': {
+        name: 'Fred',
+        year: 2000
+    },
+    '2': {
+        name: 'Inge',
+        city: 'Rome',
+        spouse: 3
+    },
+    '3': {
+        name: 'Fred',
+        city: 'Rome',
+        spouse: 2
+    }
+}
+
+const rootValue = {
+    hello: ({id}) => {
+        const person = persons[String(id)]
+        return person? person : { name: 'Not found' }
+    }
+}
+
+app.use('/graph', graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true
+}))
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
