99bf86aa40ce05866387a3de5a0e5a199d865272
Torsten Schlieder <torsten.schlieder@unite.eu>
2023-01-11 10:13:23 +0100

    Renovated author-add form

--- src/client/author-add.tsx
+++ src/client/author-add.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment, useState } from 'react'
 import { useMutation } from '@apollo/client'
-import { CREATE_AUTHOR } from './graphql'
+import { CREATE_AUTHOR, GET_AUTHORS } from './graphql'
 import './form.css'
 
 type AuthorAddProps = {
@@ -13,22 +13,25 @@ const AuthorAddButton = ({enableEdit}: AuthorAddProps): JSX.Element => (
 
 type AuthorInputProps = {
     name: string
+    birth?: string
     city?: string
 }
 
 // Derives GraphQL author input from state, which in turn captures form values
-const createAuthorInput = (formState: AuthorInputProps): AuthorInputProps => {
-    const result: AuthorInputProps = { name: formState.name }
-    if (formState.city) result.city = formState.city
+const createAuthorInput = (name: string, birth: string, city: string): AuthorInputProps => {
+    const result: AuthorInputProps = { name }
+    if (birth) result.birth = birth
+    if (city) result.city = city
     return result
 }
 
-// TODO: Switch to one useState per field
 const AuthorAddPanel = ({enableEdit}: AuthorAddProps): JSX.Element => {
-    const [formState, setFormState] = useState<AuthorInputProps>({ name: '', city: '' })
+    const [name, setName] = useState<string>('')
+    const [birth, setBirth] = useState<string>('')
+    const [city, setCity] = useState<string>('')
     const [createAuthor, { loading, error, reset }] = useMutation(CREATE_AUTHOR, {
-        variables: { authorInput: createAuthorInput(formState) },
-        refetchQueries: ['GetAuthors']
+        variables: { authorInput: createAuthorInput(name, birth, city) },
+        refetchQueries: [{query: GET_AUTHORS}]
     })
     if (loading) return <p>Submitting...</p>
     return (
@@ -38,20 +41,17 @@ const AuthorAddPanel = ({enableEdit}: AuthorAddProps): JSX.Element => {
                 createAuthor().then(() => enableEdit(false))
             }}>
                 <label>Name:</label>
-                <input type={'text'}
-                       name={'name'}
-                       value={formState.name}
-                       onChange={e => setFormState({...formState, name: e.target.value})} />
+                <input type={'text'} name={'name'} value={name} onChange={e => setName(e.target.value)} />
+
+                <label>Birth (yyyy-mm-dd):</label>
+                <input type={'text'} name={'birth'} value={birth} onChange={e => setBirth(e.target.value)} />
 
                 <label>City:</label>
-                <input type={'text'}
-                       name={'city'}
-                       value={formState.city}
-                       onChange={e => setFormState({...formState, city: e.target.value})} />
+                <input type={'text'} name={'city'} value={city} onChange={e => setCity(e.target.value)} />
 
                 <div />
                 <div>
-                    <button type="submit" disabled={!formState.name}>Add author</button>
+                    <button type="submit" disabled={!name}>Add author</button>
                     <button className='cancel-button' onClick={() => {enableEdit(false); reset()}}>Cancel</button>
                 </div>
             </form>
